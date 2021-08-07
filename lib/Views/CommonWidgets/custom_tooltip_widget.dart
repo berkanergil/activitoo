af4169d02b1744a6cada872f300b8a2c0b4281e6
GenericKnowledge <karaaegean@gmail.com>
2021-08-07 15:09:22 +0300

    add custom_tooltip_widget.dart to Common Widgets folder

---
+++ lib/Views/CommonWidgets/custom_tooltip_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class CustomTooltip extends StatelessWidget {
+  final Widget child;
+  final String message;
+  final Color color;
+
+  CustomTooltip(
+      {required this.message, required this.child, required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    final key = GlobalKey<State<Tooltip>>();
+    return Tooltip(
+      key: key,
+      message: message,
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: const BorderRadius.all(Radius.circular(4)),
+      ),
+      child: GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        onTap: () => _onTap(key),
+        child: child,
+      ),
+    );
+  }
+
+  void _onTap(GlobalKey key) {
+    final dynamic tooltip = key.currentState;
+    tooltip?.ensureTooltipVisible();
+  }
+}
